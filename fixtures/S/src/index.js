cd3a283dd05e2d7a422f08a3c4d4166900431764
Wesley Luyten <me@wesleyluyten.com>
2019-04-21 10:04:05 -0400

    Add S fixture

---
+++ fixtures/S/src/index.js
@@ -0,0 +1,22 @@
+import S from 's-js';
+import sinuous from 'sinuous';
+import htm from 'htm';
+
+const h = sinuous.bind(S);
+const html = htm.bind(h);
+
+const count = S.data(0);
+const style = S.data('');
+
+const template = () => {
+  return html`
+    <h1 style=${() => style()}>
+      Sinuous <sup>${() => count()}</sup>
+    </h1>
+  `;
+};
+
+S.root(() => document.querySelector('.sinuous').append(template()));
+setInterval(() => style({ color: randomColor() }) && count(count() + 1), 1000);
+
+const randomColor = () => '#' + ((Math.random() * (1 << 24)) | 0).toString(16);
