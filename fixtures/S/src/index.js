5bd4d7825bb41feacb48f76a234d6de39016eacc
Wesley Luyten <me@wesleyluyten.com>
2019-05-19 16:43:42 -0400

    Update fixtures

--- fixtures/S/src/index.js
+++ fixtures/S/src/index.js
@@ -1,8 +1,10 @@
 import S from 's-js';
 import sinuous from 'sinuous';
+import each from 'sinuous/each';
 import htm from 'htm';
 
-const h = sinuous(S);
+const subscribe = (fn) => S.root((dispose) => S(fn) && dispose);
+const h = sinuous({ ...S, S, subscribe });
 const html = htm.bind(h);
 const randomColor = () => '#' + ((Math.random() * (1 << 24)) | 0).toString(16);
 
@@ -13,17 +15,43 @@ const onclick = S.data(clicked);
 function clicked() {
   onclick(false);
   console.log('removed click handler');
+
+  h.cleanUp();
+  console.log('removed observers');
 }
+
+let list = S.data([
+  'bread',
+  'milk',
+  'honey',
+  'chips',
+  'cookie'
+]);
 
 const template = () => {
   return html`
-    <h1 style=${style}>
-      Sinuous <sup>${count}</sup>
-      <div>${() => count() + count()}</div>
-      <button onclick="${onclick}">Click</button>
-    </h1>
+    <div>
+      <h1 style=${style}>
+        Sinuous <sup>${count}</sup>
+        <div>${() => count() + count()}</div>
+        <button onclick="${onclick}">Click</button>
+      </h1>
+      <ul>
+        ${each(list, (item) => html`<li>${item}</li>`)}
+      </ul>
+    </div>
   `;
 };
 
-S.root(() => document.querySelector('.sinuous').append(template()));
-setInterval(() => style({ color: randomColor() }) && count(count() + 1), 1000);
+S.root(() => document.querySelector('.sinuous').appendChild(template()));
+setInterval(() => {
+  style({ color: randomColor() });
+  count(count() + 1);
+
+  const randomList = shuffle(list());
+  list(randomList);
+}, 1000);
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
--- fixtures/observable/src/index.js
+++ fixtures/observable/src/index.js
@@ -1,32 +1,52 @@
-import o, { subscribe } from 'sinuous/observable';
+import o, * as api from 'sinuous/observable';
+import each from 'sinuous/each';
 import sinuous from 'sinuous';
 
-const h = sinuous(subscribe);
+const h = sinuous(api);
 const randomColor = () => '#' + ((Math.random() * (1 << 24)) | 0).toString(16);
 
-const count = o(0);
+const count = o(5);
 const style = o({});
 const onclick = o(clicked);
+
+let list = o([
+  { id: 1, text: o('bread') },
+  { id: 2, text: o('milk') },
+  { id: 3, text: o('honey') },
+  { id: 4, text: o('chips') },
+  { id: 5, text: o('cookie') }
+]);
 
 function clicked() {
   onclick(false);
   console.log('removed click handler');
 
-  setTimeout(() => {
-    h.cleanup();
-    console.log('removed observers');
-  }, 500);
+  h.cleanUp();
+  console.log('removed observers');
 }
 
 const template = () => {
   return html`
-    <h1 style=${style}>
-      Sinuous <sup>${count}</sup>
-      <div>${() => count() + count()}</div>
-      <button onclick="${onclick}">Click</button>
-    </h1>
+    <div>
+      <h1 style=${style}>
+        Sinuous <sup>${count}</sup>
+        <div>${() => count() + count()}</div>
+        <button onclick="${onclick}">Remove click handler + stop observing</button>
+      </h1>
+      <ul>
+        ${each(list, (item) => html`<li id=${item.id}>${item.text}</li>`)}
+      </ul>
+    </div>
   `;
 };
 
 document.querySelector('.sinuous').append(template());
-setInterval(() => style({ color: randomColor() }) && count(count() + 1), 1000);
+setInterval(() => {
+  style({ color: randomColor() });
+  count(count() + 1);
+
+  list([...list(), { id: count(), text: o('cookie' + count()) }]);
+
+  const randomIndex = Math.floor(Math.random() * list().length);
+  list()[randomIndex].text('YOU WON!!!');
+}, 1000);
