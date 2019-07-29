a03c5d14e2d7fc7300e6728001cc2e17416780a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 05:03:19 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- bench/results.js
+++ bench/results.js
@@ -1,63 +1,96 @@
-/* global Plotly, sinuous */
-const { html } = sinuous;
+/* global Plotly, sinuous, observable */
+const { o, html } = sinuous;
+const { S } = observable;
 
-(async function() {
-
-const config = {
-  libs: {
-    sinuous: {
-      color: '#78FFB9'
-    },
-    vanillajs: {
-      color: '#F0DB4F'
-    },
-    react: {
-      color: '#61DAFB'
-    }
-  }
-};
-
-const response = await fetch('./results.json');
-const json = await response.json();
-const benchmarks = [
-  ...new Set(json.results.map((result) => result.benchmark))
-].sort();
-
-document.querySelector('.results').append(html`
-  ${benchmarks.map((benchmark) => html`
-    <div class="benchmark is-6 column">
-      <div id=${benchmark}></div>
-    </div>
-  `)}
-`);
-
-benchmarks.forEach(benchmark => {
-  const libs = json.results
-    .filter(result => result.benchmark === benchmark)
-    .sort((a, b) => {
-      return a.id < b.id ? 1 : -1;
-    });
-
-  let allValues = [];
-  const data = libs.map(lib => {
-    allValues = allValues.concat(lib.values);
-    return {
-      name: lib.id,
-      y: lib.values,
-      type: 'box'
-    };
-  });
-
-  const layout = {
-    title: benchmark,
-    colorway: libs.map(lib => config.libs[lib.id.split('-')[0]].color),
-    yaxis: {
-      range: [0, Math.max(...allValues)]
-    }
-  };
-
-  Plotly.newPlot(benchmark, data, layout);
+const url = o('./results.json');
+const results = o([]);
+const benchmarks = S(() => {
+  return [...new Set(results().map((result) => result.benchmark))].sort();
 });
 
+function init() {
+  S(loadResults);
 
-})(); // end async function
+  document.querySelector('.load-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    url(document.querySelector('.load-input').value);
+  });
+
+  document.querySelector('.benchmarks-list').append(html`
+    ${() => benchmarks().map((benchmark) => html`
+      <li><a href="#${benchmark}">${benchmark}</a></li>
+    `)}
+  `);
+
+  document.querySelector('.results').append(html`
+    ${() => benchmarks().map((benchmark) => html`
+      <div class="benchmark is-12 column">
+        <div id=${benchmark}></div>
+      </div>
+    `)}
+  `);
+
+  S(plotResults);
+}
+
+async function loadResults() {
+  document.querySelector('.load-input').value = url();
+
+  const response = await fetch(url());
+  const json = await response.json();
+  // krausest/js-framework-benchmark has a top level array.
+  results(Array.isArray(json) ? json : json.results);
+}
+
+function plotResults() {
+  benchmarks().forEach(benchmark => {
+    const libs = results()
+      .filter(result => result.benchmark === benchmark)
+      .sort((a, b) => {
+        return median(a.values) > median(b.values) ? 1 : -1;
+      })
+      .slice(0, 10);
+
+    let allValues = [];
+    const data = libs.map(lib => {
+      allValues = allValues.concat(lib.values);
+      return {
+        name: getId(lib),
+        y: lib.values,
+        type: 'box'
+      };
+    });
+
+    const layout = {
+      title: benchmark,
+      yaxis: {
+        range: [0, Math.max(...allValues)]
+      }
+    };
+
+    Plotly.newPlot(benchmark, data, layout);
+  });
+}
+
+init();
+
+
+function getId(lib) {
+  // krausest/js-framework-benchmark uses `framework`.
+  return lib.id || lib.framework;
+}
+
+function median(values){
+  if(values.length ===0) return 0;
+
+  values.sort(function(a,b){
+    return a-b;
+  });
+
+  var half = Math.floor(values.length / 2);
+
+  if (values.length % 2)
+    return values[half];
+
+  return (values[half - 1] + values[half]) / 2.0;
+}
