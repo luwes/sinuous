3c6a36a9e1eb3b89ad02ac33e2b3381f9b937cc8
Wesley Luyten <me@wesleyluyten.com>
2019-08-04 22:18:34 -0400

    Add more benchmarks and improve scripts

--- bench/benchmarks.js
+++ bench/benchmarks.js
@@ -1,6 +1,15 @@
 const path = require('path');
 const gzipSize = require('gzip-size');
 const u = require('./utils.js');
+const {
+  metrics,
+  testTextContains,
+  getTextByXPath,
+  clickElementByXPath,
+  testClassContains
+} = u;
+
+const WARMUP_COUNT = 5;
 
 async function benchBundleSize(page, lib) {
   const options = {
@@ -25,11 +34,11 @@ async function benchRun(page) {
   };
 
   async function run() {
-    const metrics = await u.metrics(page, async () => {
-      page.click('#add');
+    const result = await metrics(page, async () => {
+      await page.click('#add');
       await page.waitForXPath('//tbody/tr[1000]/td[2]/a');
     });
-    return metrics.time;
+    return result.time;
   }
 
   return {
@@ -44,9 +53,9 @@ async function benchReplaceAll(page) {
     label: 'replace all rows'
   };
 
-  for (let i = 0; i < 5; i++) {
-    page.click('#run');
-    await u.testTextContains(
+  for (let i = 0; i < WARMUP_COUNT; i++) {
+    await page.click('#run');
+    await testTextContains(
       page,
       '//tbody/tr[1]/td[1]',
       (i * 1000 + 1).toString()
@@ -54,11 +63,76 @@ async function benchReplaceAll(page) {
   }
 
   async function run() {
-    const metrics = await u.metrics(page, async () => {
-      page.click('#run');
-      await u.testTextContains(page, '//tbody/tr[1]/td[1]', '5001');
+    const result = await metrics(page, async () => {
+      await page.click('#run');
+      await testTextContains(page, '//tbody/tr[1]/td[1]', `${WARMUP_COUNT}001`);
     });
-    return metrics.time;
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchUpdate(page) {
+  const options = {
+    id: '03_update10th1k_x16',
+    label: 'partial update',
+    throttleCPU: 16
+  };
+
+  await page.click('#run');
+  await page.waitForXPath('//tbody/tr[1000]/td[2]/a');
+  for (let i = 0; i < 3; i++) {
+    await page.click('#update');
+    await testTextContains(
+      page,
+      '//tbody/tr[991]/td[2]/a',
+      ' !!!'.repeat(i + 1)
+    );
+  }
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await page.click('#update');
+      await testTextContains(
+        page,
+        '//tbody/tr[991]/td[2]/a',
+        ' !!!'.repeat(3 + 1)
+      );
+    });
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchSelect(page) {
+  const options = {
+    id: '04_select1k',
+    label: 'select row',
+    throttleCPU: 16
+  };
+
+  await page.click('#run');
+  await page.waitForXPath('//tbody/tr[1]/td[2]/a');
+  for (let i = 0; i < WARMUP_COUNT; i++) {
+    await page.click('#update');
+    await clickElementByXPath(page, `//tbody/tr[${i + 1}]/td[2]/a`);
+  }
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await page.click('#update');
+      await clickElementByXPath(page, `//tbody/tr[2]/td[2]/a`);
+      await testClassContains(page, "//tbody/tr[2]", "danger");
+    });
+    return result.time;
   }
 
   return {
@@ -70,24 +144,123 @@ async function benchReplaceAll(page) {
 async function benchSwapRows(page) {
   const options = {
     id: '05_swap1k',
-    label: 'swap rows'
+    label: 'swap rows',
+    throttleCPU: 4
   };
 
-  page.click('#run');
+  await page.click('#run');
   await page.waitForXPath('//tbody/tr[1]/td[2]/a');
-  for (let i = 0; i < 5; i++) {
-    let text = await u.getTextByXPath(page, '//tbody/tr[2]/td[2]/a');
-    page.click('#swaprows');
-    await u.testTextContains(page, '//tbody/tr[999]/td[2]/a', text);
+  for (let i = 0; i < WARMUP_COUNT; i++) {
+    let text = await getTextByXPath(page, '//tbody/tr[2]/td[2]/a');
+    await page.click('#swaprows');
+    await testTextContains(page, '//tbody/tr[999]/td[2]/a', text);
   }
 
   async function run() {
-    const metrics = await u.metrics(page, async () => {
-      let text = await u.getTextByXPath(page, '//tbody/tr[2]/td[2]/a');
-      page.click('#swaprows');
-      await u.testTextContains(page, '//tbody/tr[999]/td[2]/a', text);
+    const result = await metrics(page, async () => {
+      let text = await getTextByXPath(page, '//tbody/tr[2]/td[2]/a');
+      await page.click('#swaprows');
+      await testTextContains(page, '//tbody/tr[999]/td[2]/a', text);
     });
-    return metrics.time;
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchRemove(page) {
+  const options = {
+    id: '06_remove-one-1k',
+    label: 'remove row'
+  };
+
+  await page.click('#run');
+  await page.waitForXPath("//tbody/tr[1]/td[2]/a");
+  for (let i = 0; i < WARMUP_COUNT; i++) {
+    await testTextContains(page, `//tbody/tr[${WARMUP_COUNT - i + 4}]/td[1]`, (WARMUP_COUNT - i + 4).toString());
+    await clickElementByXPath(page, `//tbody/tr[${WARMUP_COUNT - i + 4}]/td[3]/a/span[1]`);
+    await testTextContains(page, `//tbody/tr[${WARMUP_COUNT - i + 4}]/td[1]`, '10');
+  }
+  await testTextContains(page, '//tbody/tr[5]/td[1]', '10');
+  await testTextContains(page, '//tbody/tr[4]/td[1]', '4');
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await clickElementByXPath(page, "//tbody/tr[4]/td[3]/a/span[1]");
+      await testTextContains(page, '//tbody/tr[4]/td[1]', '10');
+    });
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchRunBig(page) {
+  const options = {
+    id: '07_create10k',
+    label: 'create 10,000 rows'
+  };
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await page.click('#runlots');
+      await page.waitForXPath('//tbody/tr[10000]/td[2]/a');
+    });
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchAppendToManyRows(page) {
+  const options = {
+    id: '08_create1k-after1k_x2',
+    label: 'append rows to large table',
+    throttleCPU: 2
+  };
+
+  await page.click('#run');
+  await page.waitForXPath('//tbody/tr[1000]/td[2]/a');
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await page.click('#add');
+      await page.waitForXPath('//tbody/tr[1100]/td[2]/a');
+    });
+    return result.time;
+  }
+
+  return {
+    ...options,
+    run
+  };
+}
+
+async function benchClear(page) {
+  const options = {
+    id: '09_clear1k_x8',
+    label: 'clear rows',
+    throttleCPU: 8
+  };
+
+  await page.click('#run');
+  await page.waitForXPath('//tbody/tr[1000]/td[2]/a');
+
+  async function run() {
+    const result = await metrics(page, async () => {
+      await page.click('#clear');
+      await page.waitForXPath('//tbody/tr[1]', { hidden: true });
+    });
+    return result.time;
   }
 
   return {
@@ -97,5 +270,16 @@ async function benchSwapRows(page) {
 }
 
 module.exports = {
-  benchmarks: [benchBundleSize, benchRun, benchReplaceAll, benchSwapRows]
+  benchmarks: [
+    benchBundleSize,
+    benchRun,
+    benchReplaceAll,
+    benchUpdate,
+    benchSelect,
+    benchSwapRows,
+    benchRemove,
+    benchRunBig,
+    benchAppendToManyRows,
+    benchClear
+  ]
 };
