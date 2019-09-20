3cfc4476578b6c28738b2e5e3c33c4d8706722ba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-20 03:08:09 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- packages/sinuous/template/src/template.js
+++ packages/sinuous/template/src/template.js
@@ -26,7 +26,7 @@ export function t(key) {
  */
 export function o(key) {
   const observedTag = t(key);
-  observedTag._observable = 1;
+  observedTag._observable = true;
   return observedTag;
 }
 
@@ -72,6 +72,8 @@ export function template(fn) {
   function clone(props) {
     const keyedActions = {};
     const el = fragment.cloneNode(true);
+
+    // Set a custom property `props` for easy access to the passed argument.
     el.firstChild.props = props;
 
     for (let i = 0; i < cloneActions.length; i++) {
