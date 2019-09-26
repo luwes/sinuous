38ecaf1077eb894458f568ce6a0a44acba1f53a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-26 13:24:47 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

---
+++ packages/sinuous/h/src/property.js
@@ -0,0 +1,65 @@
+import { api } from './api.js';
+
+export function property(name, value, el, isAttr, isCss) {
+  if (!name || (name === 'attrs' && (isAttr = true))) {
+    for (name in value) {
+      property(name, value[name], el, isAttr, isCss);
+    }
+  } else if (name[0] === 'o' && name[1] === 'n' && !value.$o) {
+    // Functions added as event handlers are not executed
+    // on render unless they have an observable indicator.
+    handleEvent(el, name, value);
+  } else if (typeof value === 'function') {
+    if (value.$t) {
+      // Record property action in template.
+      value.$t(2, property, el, name);
+    } else {
+      api.subscribe(() => {
+        property(name, value(), el, isAttr, isCss);
+      });
+    }
+  } else if (isCss) {
+    el.style.setProperty(name, value);
+  } else if (
+    isAttr ||
+    name.slice(0, 5) === 'data-' ||
+    name.slice(0, 5) === 'aria-'
+  ) {
+    el.setAttribute(name, value);
+  } else if (name === 'style') {
+    if (typeof value === 'string') {
+      el.style.cssText = value;
+    } else {
+      property(null, value, el, isAttr, true);
+    }
+  } else {
+    if (name === 'class') name += 'Name';
+    el[name] = value;
+  }
+}
+
+function handleEvent(el, name, value) {
+  name = name.slice(2);
+
+  const removeListener = api.cleanup(() =>
+    el.removeEventListener(name, eventProxy)
+  );
+
+  if (value) {
+    el.addEventListener(name, eventProxy);
+  } else {
+    removeListener();
+  }
+
+  (el._listeners || (el._listeners = {}))[name] = value;
+}
+
+/**
+ * Proxy an event to hooked event handlers.
+ * @param {Event} e - The event object from the browser.
+ * @return {Function}
+ */
+function eventProxy(e) {
+  // eslint-disable-next-line
+  return this._listeners[e.type](e);
+}
