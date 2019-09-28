4b0250ee5163745e1dcf09f68fe8791b19def58f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-28 19:22:43 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- bench/results/helpers.js
+++ bench/results/helpers.js
@@ -4,13 +4,23 @@ export const colors = {
   sinuous: '#70EDAC',
   solid: '#481B82',
   surplus: '#C559D4',
-  react: '#61DAFB',
-  mikado: '#59ABE9',
+  ivi: '#FF3800',
   stage0: '#374838',
   domc: '#704B38',
+  hyperapp: '#037FFF',
+  preact: '#673AB8',
+  react: '#61DAFB',
+  mikado: '#59ABE9',
   inferno: '#E41E1D',
   wasm: '#644FF0',
-  fidan: '#2B7489'
+  fidan: '#2B7489',
+  vue: '#40B883',
+  angular: '#A6130C',
+  ember: '#E04E39',
+  svelte: '#FF3E00',
+  lit: '#FF6C6A',
+  mithril: '#1E5799',
+  sifrr: '#364F6B'
 };
 
 export function getId(lib) {
