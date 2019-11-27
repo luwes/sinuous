7de5da5cfeea252f40b6416544de8bc0b3551393
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-27 16:00:27 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- site/assets/src/header.js
+++ site/assets/src/header.js
@@ -1,4 +1,3 @@
-/* global Prism */
 import { observable } from 'sinuous';
 import { hydrate, h } from 'sinuous/hydrate';
 import { ready } from './utils.js';
@@ -14,10 +13,4 @@ ready(() => {
   hydrate(
     html`<a class="navbar-menu${isActive}" />`
   );
-});
-
-!function(){if("undefined"!=typeof self&&self.Prism||"undefined"!=typeof global&&global.Prism){var n=function(n){return n},s={classMap:n,prefixString:""};Prism.plugins.customClass={map:function(i){s.classMap="function"==typeof i?i:function(n){return i[n]||n}},prefix:function(n){s.prefixString=n}},Prism.hooks.add("wrap",function(i){(s.classMap!==n||s.prefixString)&&(i.classes=i.classes.map(function(n){return s.prefixString+s.classMap(n,i.language)}))})}}();
-
-Prism.plugins.customClass.map({
-  tag: 'node'
 });
