da0752060cec9aaa02cd73a94428542222601c7b
Wesley Luyten <me@wesleyluyten.com>
2020-01-17 14:41:38 -0500

    Rewrite memo to keep a permanent cache instead of only checking the last result

--- packages/sinuous/memo/test/memo.js
+++ packages/sinuous/memo/test/memo.js
@@ -28,69 +28,23 @@ test('memo with functions', t => {
   t.end();
 });
 
-test('memo with valueEquals override', t => {
+test('memo with functions in objects', t => {
   // a rather absurd equals operation we can verify in tests
   let called = 0;
-  const valueEquals = (a, b) => typeof a === typeof b;
+  const fn1 = (a) => a;
+  const fn2 = (a) => a;
+  const obj1 = { fn1 };
+  const obj2 = { fn2 };
   const memoized = memo(a => {
     called++;
     return a;
-  }, valueEquals);
-  t.equal(memoized(1), 1);
-  t.equal(memoized(2), 1); // yes, really true
+  });
+  t.equal(memoized(obj1), obj1);
   t.equal(called, 1);
-  t.equal(memoized('A'), 'A');
+  t.equal(memoized({ fn1 }), obj1);
+  t.equal(called, 1);
+  t.equal(memoized(obj2), obj2);
   t.equal(called, 2);
-  t.end();
-});
-
-test('memo passes correct objects to equalityCheck', t => {
-  let fallthroughs = 0;
-  function shallowEqual(newVal, oldVal) {
-    if (newVal === oldVal) return true;
-
-    fallthroughs += 1; // code below is expensive and should be bypassed when possible
-
-    let countA = 0;
-    let countB = 0;
-    for (let key in newVal) {
-      if (
-        Object.hasOwnProperty.call(newVal, key) &&
-        newVal[key] !== oldVal[key]
-      )
-        return false;
-      countA++;
-    }
-    for (let key in oldVal) {
-      if (Object.hasOwnProperty.call(oldVal, key)) countB++;
-    }
-    return countA === countB;
-  }
-
-  const someObject = { foo: 'bar' };
-  const anotherObject = { foo: 'bar' };
-  const memoized = memo(a => a, shallowEqual);
-
-  // the first call to `memoized` doesn't hit because `memo.lastArgs` is uninitialized
-  // and so `equalityCheck` is never called
-  memoized(someObject);
-  t.equal(fallthroughs, 0, 'first call does not shallow compare');
-
-  // the next call, with a different object reference, does fall through
-  memoized(anotherObject);
-  t.equal(fallthroughs, 1, 'call with different object does shallow compare');
-
-  // the third call does not fall through because `memo` passes `anotherObject` as
-  // both the `newVal` and `oldVal` params. This allows `shallowEqual` to be much more performant
-  // than if it had passed `someObject` as `oldVal`, even though `someObject` and `anotherObject`
-  // are shallowly equal
-  memoized(anotherObject);
-  t.equal(
-    fallthroughs,
-    1,
-    'call with same object as previous call does not shallow compare'
-  );
-
   t.end();
 });
 
