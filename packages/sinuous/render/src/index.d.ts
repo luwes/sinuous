bcb5b0cffb7ca63173a90ef4718c68515a51c9ce
Wesley Luyten <me@wesleyluyten.com>
2020-01-27 19:39:50 -0500

    Add types

--- packages/sinuous/render/src/index.d.ts
+++ packages/sinuous/render/src/index.d.ts
@@ -1,16 +1,74 @@
 import { JSXInternal } from '../../src/jsx';
+import { Observable } from '../../observable/src';
 
-type Props =
-  | JSXInternal.HTMLAttributes &
-    Record<string, any>
-  | null
-
-type Child =
-  | Node
-  | Node[]
-
-interface Func {
-  (props: Props): Child
+interface TemplateResult<P = {}> {
+  (): Node
 }
 
-export function memo<T>(func: Func): Func;
+type ElementChild =
+  | TemplateResult
+  | Function
+  | Observable<any>
+  | object
+  | string
+  | number
+  | boolean
+  | null
+  | undefined;
+type ElementChildren = ElementChild[] | ElementChild;
+
+interface FunctionComponent<P = {}> {
+  (props: object, ...children: ElementChildren[]): any
+  (...children: ElementChildren[]): any
+}
+
+export function render(delta: TemplateResult, root?: Node): Node;
+
+export const rhtml: (strings: TemplateStringsArray, ...values: any[]) => TemplateResult;
+export const rsvg: (strings: TemplateStringsArray, ...values: any[]) => TemplateResult;
+
+export function r(
+  type: string,
+  props:
+    | JSXInternal.HTMLAttributes &
+      Record<string, any>
+    | null,
+  ...children: ElementChildren[]
+): TemplateResult;
+export function r(
+  type: FunctionComponent,
+  props:
+    | JSXInternal.HTMLAttributes &
+      Record<string, any>
+    | null,
+  ...children: ElementChildren[]
+): TemplateResult;
+export function r(
+  children: ElementChildren[]
+): TemplateResult;
+export namespace r {
+  export import JSX = JSXInternal;
+}
+
+export function rs(
+  type: string,
+  props:
+    | JSXInternal.SVGAttributes &
+      Record<string, any>
+    | null,
+  ...children: ElementChildren[]
+): TemplateResult;
+export function rs(
+  type: FunctionComponent,
+  props:
+    | JSXInternal.SVGAttributes &
+      Record<string, any>
+    | null,
+  ...children: ElementChildren[]
+): TemplateResult;
+export function rs(
+  children: ElementChildren[]
+): TemplateResult;
+export namespace rs {
+  export import JSX = JSXInternal;
+}
