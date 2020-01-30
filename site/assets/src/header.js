d07615b2b8336a144a371c208253df509ade608a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-30 15:00:07 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- site/assets/src/header.js
+++ site/assets/src/header.js
@@ -1,16 +1,16 @@
 import { observable } from 'sinuous';
-import { hydrate, h } from 'sinuous/hydrate';
+import { hydrate, d } from 'sinuous/hydrate';
 import { ready } from './utils.js';
 
 ready(() => {
   const isActive = observable('');
 
   hydrate(
-    html`<a class="navbar-burger burger${isActive}"
+    dhtml`<a class="navbar-burger burger${isActive}"
       onclick=${() => isActive(!isActive() ? ' is-active' : '')} />`
   );
 
   hydrate(
-    html`<a class="navbar-menu${isActive}" />`
+    dhtml`<a class="navbar-menu${isActive}" />`
   );
 });
