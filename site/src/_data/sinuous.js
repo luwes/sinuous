c4243cc6d6eba4a5475a7f98eb4f60dbbf71874e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-25 23:09:15 +0000

    Deploy luwes/sinuous to github.com/luwes/sinuous.git:gh-pages

--- site/src/_data/sinuous.js
+++ site/src/_data/sinuous.js
@@ -3,7 +3,7 @@ const Cache = require("@11ty/eleventy-cache-assets");
 module.exports = async function() {
   // https://developer.github.com/v3/repos/#get
   let json = await Cache("https://raw.githubusercontent.com/luwes/sinuous/master/packages/sinuous/package.json", {
-    duration: "1d", // 1 day
+    duration: "15m",
     type: "json" // also supports "text" or "buffer"
   });
 
