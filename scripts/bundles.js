d487de6002daec63d67354d405c07de2a0ad1d7c
Gen Hames <hamesg@tutanota.de>
2020-05-28 16:22:19 -0700

    Unroll `output` instead of the entire bundle config
    Unfortunately, after all the work I discover it's not the same.
    
    Plugins are "build time" or "output time".

--- scripts/bundles.js
+++ scripts/bundles.js
@@ -1,3 +1,18 @@
+import path from 'path';
+import replace from '@rollup/plugin-replace'; // TODO: Replace this
+import nodeResolve from '@rollup/plugin-node-resolve';
+import bundleSize from 'rollup-plugin-size';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * @typedef {import("rollup").ModuleFormat} ModuleFormat
+ * @typedef {import("rollup").MergedRollupOptions} MergedRollupOptions
+ * @typedef {import("rollup").OutputOptions} OutputOptions
+ * @typedef {import("rollup").Plugin} Plugin
+ */
+
+/** @type {{ [key: string]: ModuleFormat }} */
 const bundleFormats = {
   CJS:  'cjs',
   ESM:  'esm',
@@ -6,8 +21,10 @@ const bundleFormats = {
 };
 
 const { CJS, ESM, IIFE, UMD } = bundleFormats;
+const src = 'packages/sinuous';
+const dest = (name, format) => `dist/${format}/${name}.${ext[format]}`;
 
-// Format extensions, see bundles#file
+// Format extensions
 const ext = {
   [CJS ]: 'js',
   [ESM ]: 'js',
@@ -15,162 +32,269 @@ const ext = {
   [UMD ]: 'js',
 };
 
-/**
- * @typedef {(code: string) => string} Replacer
- * @type {(from: string, to: string) => Replacer}
- */
-// const replaceImport = (from, to) =>
-//   (code) => code.replace(`from '${from}'`, `from '${to}'`);
+// Store these for filtering by CLI
+/** @type {string[]} */
+const bundleNames = [];
 
-/**
- * @typedef Bundle
- * Required
- * @property {string} input  Rollup's input - modified later
- * @property {string} file   Rollup's output.file - modified later
- * @property {string} format Rollup's output.format (ESM/UDM/IIFE)
- * Optional
- * @property {string}     [name]      Rollup's output.name
- * @property {boolean}    [extend]    Extend rather than replace global [name]
- * @property {boolean}    [sourcemap] Emit sourcemaps
- * @property {string[]}   [external]  Skipped dependencies
- * @property {Replacer[]} [replace]   Bundle string replacement functions
- */
-
-/** @type {Bundle[]} */
-const bundleConfig = [
+/** @type {MergedRollupOptions[]} */
+const bundles = [
   // `htm` has to come before `babel-plugin-htm`
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'htm/src/index.js',
-    file: 'htm',
-    format,
-    name: 'htm',
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'hydrate/src/index.js',
-    file: 'hydrate',
-    format,
-    name: 'hydrate',
+  {
+    input: `${src}/htm/src/index.js`,
+    output: out('htm', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'htm' },
+    }),
+  },
+  {
+    input: `${src}/hydrate/src/index.js`,
     external: ['sinuous', 'sinuous/htm'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'observable/src/observable.js',
-    file: 'observable',
-    format,
-    name: 'observable',
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'h/src/index.js',
-    file: 'h',
-    format,
-    name: 'h',
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'template/src/template.js',
-    file: 'template',
-    format,
-    name: 'template',
+    output: out('hydrate', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'hydrate' },
+    }),
+  },
+  {
+    input: `${src}/observable/src/observable.js`,
+    output: out('observable', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'observable' },
+    }),
+  },
+  {
+    input: `${src}/h/src/index.js`,
+    output: out('sinuous', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'h' },
+    }),
+  },
+  {
+    input: `${src}/template/src/template.js`,
     external: ['sinuous'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'data/src/data.js',
-    file: 'data',
-    format,
-    name: 'data',
+    output: out('template', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'template' },
+    }),
+  },
+  {
+    input: `${src}/data/src/data.js`,
     external: ['sinuous', 'sinuous/template'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'memo/src/memo.js',
-    file: 'memo',
-    format,
-    name: 'memo',
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'render/src/index.js',
-    file: 'render',
-    format,
-    name: 'render',
+    output: out('data', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'data' },
+    }),
+  },
+  {
+    input: `${src}/memo/src/memo.js`,
+    output: out('memo', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'memo' },
+    }),
+  },
+  {
+    input: `${src}/render/src/index.js`,
     external: ['sinuous', 'sinuous/template', 'sinuous/htm'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'map/mini/src/mini.js',
-    file: 'map/mini',
-    format,
-    name: 'mini',
+    output: out('render', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'render' },
+    }),
+  },
+  {
+    input: `${src}/map/mini/src/mini.js`,
     external: ['sinuous'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'map/src/index.js',
-    file: 'map',
-    format,
-    name: 'map',
+    output: out('map/mini', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'mini' },
+    }),
+  },
+  {
+    input: `${src}/map/src/index.js`,
     external: ['sinuous'],
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
-    input: 'src/index.js',
-    file: 'sinuous',
-    format,
-    name: 'sinuous',
+    output: out('map', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'map' },
+    }),
+  },
+  {
+    input: `${src}/src/index.js`,
     external: ['sinuous/observable', 'sinuous/htm' ],
-  })),
-
-  ...[ESM].map(format => ({
-    input: 'src/index.js',
-    file: 'sinuous-observable',
-    format,
-    // Only used to display bundle size, `observable` is a peer dependency to
-    // avoid issues with the global `tracking` variable.
-    name: 'so',
+    output: out('sinuous', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'sinuous' },
+    }),
+  },
+  {
+    input: `${src}/src/index.js`,
     external: ['sinuous/htm'],
-    sourcemap: false,
-  })),
-
-  ...[ESM, CJS].map(format => ({
-    input: 'babel-plugin-htm/src/index.js',
-    file: 'babel-plugin-htm',
-    format,
-  })),
-
-  ...[ESM, UMD, IIFE].map(format => ({
+    output: out('sinuous-observable', {
+      sourcemap: false,
+      [ESM]: {},
+    }),
+  },
+  {
+    input: `${src}/babel-plugin-htm/src/index.js`,
+    output: out('babel-plugin-htm', {
+      [ESM + CJS]: {},
+    }),
+  },
+  {
     // Multiple globals - @see https://github.com/rollup/rollup/issues/494
-    input: 'all/src/index.js',
-    file: 'all',
-    format,
-    name: 'window',
-    extend: true,
-  })),
+    input: `${src}/all/src/index.js`,
+    output: out('all', {
+      [ESM]: {},
+      [UMD + IIFE]: { name: 'window', extend: true },
+    }),
+  },
 ];
 
-// Quick tune ups
-const bundles = bundleConfig.map(config => {
-  const defaults = {
-    name: undefined,
-    extend: false,
-    sourcemap: false,
-    external: [],
-    replace: [],
+/**
+ * @typedef { OutputOptions & { [maybeFormat: string]: (OutputOptions | {}) }} PerFormat
+ * @type {(name: string, perFormat: PerFormat) => OutputOptions[]}
+ */
+function out(name, perFormat) {
+  // Unfortunately this is the only reference to name...
+  bundleNames.push(name);
+
+  const globalOptions = {};
+  const outputConfigs = [];
+  for (const key in perFormat) {
+    let keyIsFormat = false;
+    for (const format of Object.values(bundleFormats)) {
+      if (key.includes(format)) {
+        outputConfigs.push({
+          file: dest(name, format),
+          format,
+          ...perFormat[key],
+        });
+        keyIsFormat = true;
+      }
+    }
+    if (keyIsFormat === false) {
+      globalOptions[key] = perFormat[key];
+    }
+  }
+  return outputConfigs.map(o => Object.assign({}, globalOptions, o));
+}
+
+// Tweaks and global changes. This finalizes the configs
+bundles.forEach(bundle => {
+  bundle.watch = {
+    clearScreen: false,
   };
-  // Set all optional properties
-  config = Object.assign(defaults, config);
+  // Global plugins not per format
+  bundle.plugins = [
+    nodeResolve(),
+  ];
+  bundle.output.forEach(o => {
+    o.plugins = [
+      bundleSize({
+        // Unfortunately this package wasn't properly typed or documented, see
+        // package `size-plugin-core` for all available options
+        // @ts-ignore
+        columnWidth: 25,
+        decorateItem: (item) =>
+          item.replace('.js', `.js ${o.format.toUpperCase().padEnd(4)}`),
+      }),
 
-  // Prefix all input and output paths
-  const { format } = config;
-  config.input = `packages/sinuous/${config.input}`;
-  config.file = `dist/${format}/${config.file}.${ext[format]}`;
+      [ESM, UMD, IIFE].includes(o.format)
+        && pluginTerser(),
+      // TODO: Brutal
+      // [UMD, IIFE].includes(o.format)
+      //   && pluginBabel(),
+      // [ESM].includes(o.format)
+      //   && pluginReplaceESM(bundle),
 
-  // Patch all bundle replacements
-  // TODO: config.output[ESM].plugins.push(replace())
+      ...(o.plugins || []),
+    ]
+      // Allow plugins to return an array
+      .flat()
+      .filter(Boolean);
 
-  return config;
+    o.strict = false;   // Remove `use strict;`
+    o.interop = false;  // Remove `r=r&&r.hasOwnProperty("default")?r.default:r;`
+    o.freeze = false;   // Remove `Object.freeze()`
+    o.esModule = false; // Remove `esModule` property
+  });
+  bundle.onwarn = (warning) => {
+    // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+    const skip = [
+      'THIS_IS_UNDEFINED',
+      'UNKNOWN_OPTION',
+      'MISSING_GLOBAL_NAME',
+      'CIRCULAR_DEPENDENCY',
+    ];
+    if (skip.includes(warning.code)) {
+      return;
+    }
+    console.error(warning.message);
+  };
 });
 
-export { bundles, bundleFormats };
+/** @type {() => Plugin} */
+function pluginBabel() {
+  // TODO: Brutal...
+  return getBabelOutputPlugin({
+    babelHelpers: 'bundled',
+  });
+}
+
+/** @type {() => Plugin} */
+function pluginTerser() {
+  return terser({
+    sourcemap: true,
+    ecma: 2017,
+    warnings: true,
+    compress: {
+      passes: 2,
+    },
+    mangle: {
+      properties: {
+        regex: /^_/,
+      },
+    },
+    nameCache: {
+      props: {
+        cname: 6,
+        props: {
+          $_tag: '__t',
+          $_props: '__p',
+          $_children: '__c',
+          // Fixes a weird issue with mangling. `r.o.has` is not a function.
+          $_observers: '__o',
+        },
+      },
+    },
+  });
+}
+
+/** @type {(bundleConfig: MergedRollupOptions) => Plugin[]?} */
+function pluginReplaceESM(bundleConfig) {
+  const { external } = bundleConfig;
+  if (!external || !Array.isArray(external) || external.length === 0) {
+    return;
+  }
+  const { format, file } = bundleConfig.output.find(o => o.format === ESM);
+  const newExternals = [];
+  const newPlugins = [];
+
+  for (const dep of external) {
+    // @ts-ignore Externals can be regular expressions :(
+    const depFile = `dist/${format}/${path.basename(dep)}.js`;
+    const depPath = path.relative(file, depFile)
+      .replace('..', '.')
+      .replace('./..', '..');
+    // TODO: config.replace.push(replaceImport(dep, ''));
+    newPlugins.push(
+      replace({
+        delimiters: ['', ''],
+        [`from '${dep}'`]: `from '${depPath}'`,
+      })
+    );
+    // Also mark itself as a dependency
+    newExternals.push(depPath);
+  }
+  external.push(...newExternals);
+  return newPlugins;
+}
+
+export { bundles, bundleNames, bundleFormats };
--- scripts/rollup.js
+++ scripts/rollup.js
@@ -1,19 +1,5 @@
-import path from 'path';
-import babel from '@rollup/plugin-babel';
-// TODO: Replace this
-import replace from '@rollup/plugin-replace';
-import nodeResolve from '@rollup/plugin-node-resolve';
-
-import bundleSize from 'rollup-plugin-size';
-import gzip from 'rollup-plugin-gzip';
-import { terser } from 'rollup-plugin-terser';
-
 import minimist from 'minimist';
-
-import { bundleFormats, bundles } from './bundles.js';
-// TODO: import { replace } from './rollup-replace.js';
-
-const { CJS, ESM, IIFE, UMD } = bundleFormats;
+import { bundles, bundleNames } from './bundles.js';
 const argv = minimist(process.argv.slice(2));
 
 const requestedBundleNames
@@ -30,117 +16,17 @@ const requestedBundleTypes
     : [];
 
 // For every type in bundle.types creates a new bundle obj.
-const allBundles = bundles.filter(({ name, format }) => {
+const allBundles = bundles.filter((bundle, index) => {
+  const name = bundleNames[index];
   if (requestedBundleNames.length > 0 && !requestedBundleNames.includes(name))
     return false;
-  if (requestedBundleTypes.length > 0 && !requestedBundleTypes.includes(format))
-    return false;
-  return true;
+  if (requestedBundleTypes.length === 0)
+    return true;
+
+  bundle.output = bundle.output.filter(
+    ({ format }) => requestedBundleTypes.includes(format));
+  // Are any left after filtering?
+  return bundle.output.length > 0;
 });
 
-function getConfig(options) {
-  const {
-    name,
-    file,
-    input,
-    exports,
-    format,
-    external,
-    sourcemap,
-    extend,
-  } = options;
-
-  const config = {
-    input,
-    external,
-    watch: {
-      clearScreen: false,
-    },
-    output: {
-      format,
-      sourcemap,
-      extend,
-      file,
-      name,
-      exports,
-      strict:   false, // Remove `use strict;`
-      interop:  false, // Remove `r=r&&r.hasOwnProperty("default")?r.default:r;`
-      freeze:   false, // Remove `Object.freeze()`
-      esModule: false, // Remove `esModule` property
-    },
-    plugins: [
-      // Unfortunately this package wasn't properly typed or documented, see
-      // package `size-plugin-core` for all available options
-      bundleSize({
-        columnWidth: 25,
-        decorateItem: (item) =>
-          item.replace('.js', `.js ${format.toUpperCase().padEnd(4)}`),
-      }),
-      nodeResolve(),
-      [UMD, IIFE].includes(format)
-        && babel({
-          babelHelpers: 'bundled',
-        }),
-      [ESM, UMD, IIFE].includes(format)
-        && terser({
-          sourcemap: true,
-          ecma: 2017,
-          warnings: true,
-          compress: {
-            passes: 2,
-          },
-          mangle: {
-            properties: {
-              regex: /^_/,
-            },
-          },
-          nameCache: {
-            props: {
-              cname: 6,
-              props: {
-                $_tag: '__t',
-                $_props: '__p',
-                $_children: '__c',
-
-                // Fixes a weird issue with mangling. `r.o.has` is not a function.
-                $_observers: '__o',
-              },
-            },
-          },
-        }),
-      // TODO: options.replace && replace(options.replace),
-      options.gzip && gzip(),
-    ].filter(Boolean),
-    onwarn(warning) {
-      // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
-      const skip = ['THIS_IS_UNDEFINED', 'UNKNOWN_OPTION', 'MISSING_GLOBAL_NAME', 'CIRCULAR_DEPENDENCY'];
-      if (skip.includes(warning.code)) return;
-      console.error(warning.message);
-    },
-  };
-
-  // Fix import paths
-  if (format === ESM && external.length > 0) {
-    const externalGenerated = [];
-    for (const dep of external) {
-      const depFile = `dist/${format}/${path.basename(dep)}.js`;
-      const depPath = path.relative(config.output.file, depFile)
-        .replace('..', '.')
-        .replace('./..', '..');
-      // TODO: config.replace.push(replaceImport(dep, ''));
-      config.plugins.push(
-        replace({
-          delimiters: ['', ''],
-          [`from '${dep}'`]: `from '${depPath}'`,
-        })
-      );
-      // Also mark itself as a dependency
-      externalGenerated.push(depPath);
-    }
-    config.external.push(...externalGenerated);
-  }
-
-  return config;
-}
-
-export default allBundles.map(getConfig);
+export default allBundles;
